9574c7f0d52f391dbea0407a922d5c7bb67167f8
张世泽 <814135975@qq.com>
2018-02-05 23:49:51 +0800

    cod

---
+++ 231.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 0:
+            return False
+        if n & (n - 1) == 0:
+            return True
+        return False
