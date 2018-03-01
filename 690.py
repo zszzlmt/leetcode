92d393d5fadf45524649fa7bc17e9a441ddde936
Zach <zszv587@zju.edu.cn>
2018-03-01 20:37:13 +0800

    code

---
+++ 690.py
@@ -0,0 +1,21 @@
+"""
+# Employee info
+class Employee(object):
+    def __init__(self, id, importance, subordinates):
+        # It's the unique id of each node.
+        # unique id of this employee
+        self.id = id
+        # the importance value of this employee
+        self.importance = importance
+        # the id of direct subordinates
+        self.subordinates = subordinates
+"""
+class Solution(object):
+    def getImportance(self, employees, id):
+        """
+        :type employees: Employee
+        :type id: int
+        :rtype: int
+        """
+        dic = {item.id: item for item in employees
+               }
