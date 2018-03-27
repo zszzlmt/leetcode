19b752c88ed5f1f7992ae70b9e47aa40d4a36d97
zszzlmt <814135975@qq.com>
2018-03-27 23:09:29 +0800

    code

--- 720.py
+++ 720.py
@@ -5,12 +5,11 @@ class Solution(object):
 
     valid = None
 
-    def __traval__(self, l):
-        for word in d[l]:
-            for next in d[l + 1]:
-                if next[:-1] == word:
-                    self.valid.add(next)
-                    self.__traval__(next, l + 1)
+    def __traval__(self, word, l, d):
+        self.valid.add(word)
+        for next in d[l + 1]:
+            if next[:-1] == word:
+                self.__traval__(next, l + 1, d)
 
     def longestWord(self, words):
         """
@@ -18,12 +17,11 @@ class Solution(object):
         :rtype: str
         """
         d = defaultdict(list)
-        valid = set()
+        self.valid = set()
         for word in words:
             d[len(word)].append(word)
         for word in d[1]:
-            self.valid.add(word)
-            self.__traval__(1)
+            self.__traval__(word, 1, d)
         valid = list(self.valid)
         max_len = max([len(word) for word in valid])
         res = list()
